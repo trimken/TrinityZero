c6d0ab6765cde0486b113533ea23e3f433d482f7
Maddogz <Maddogz@localhost>
2009-05-01 13:01:24 -0400

    *Use canattack to check if canattack in pethandler and combathandler. by megamage

--- src/game/PetHandler.cpp
+++ src/game/PetHandler.cpp
@@ -106,17 +106,17 @@ void WorldSession::HandlePetAction( WorldPacket & recv_data )
                         return;
                     }
                     // only place where pet can be player
-                    pet->clearUnitState(UNIT_STAT_FOLLOW);
                     Unit *TargetUnit = ObjectAccessor::GetUnit(*_player, guid2);
                     if(!TargetUnit)
                         return;
 
-                    // not let attack friendly units.
-                    if(GetPlayer()->IsFriendlyTo(TargetUnit))
+                    if(!pet->canAttack(TargetUnit))
                         return;
+
                     // Not let attack through obstructions
                     //if(!pet->IsWithinLOSInMap(TargetUnit))
                     //    return;
+                    pet->clearUnitState(UNIT_STAT_FOLLOW);
 
                     if(pet->GetTypeId() != TYPEID_PLAYER && ((Creature*)pet)->IsAIEnabled)
                     {
