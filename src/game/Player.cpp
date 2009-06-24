0c2e1ff1999c598246c9e747c3219bf83ba5c11c
krimp <krimp@localhost>
2009-06-24 20:49:48 +0200

    + Fixes client crash after releasing spirit and getting teleported to graveyard.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -4147,15 +4147,9 @@ void Player::RepopAtGraveyard()
     // and don't show spirit healer location
     if(ClosestGrave)
     {
+		if(isDead())
+		{
         TeleportTo(ClosestGrave->map_id, ClosestGrave->x, ClosestGrave->y, ClosestGrave->z, GetOrientation());
-        if(isDead())                                        // not send if alive, because it used in TeleportTo()
-        {
-            WorldPacket data(SMSG_DEATH_RELEASE_LOC, 4*4);  // show spirit healer position on minimap
-            data << ClosestGrave->map_id;
-            data << ClosestGrave->x;
-            data << ClosestGrave->y;
-            data << ClosestGrave->z;
-            GetSession()->SendPacket(&data);
         }
     }
 }
