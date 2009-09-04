4eca2624069f636cae98182147c3cda5874ac61e
Chaz Brown <iamparadox@netscape.net>
2009-09-04 19:51:17 -0400

    Fix a couple of small typos.

--- src/game/Level3.cpp
+++ src/game/Level3.cpp
@@ -4850,7 +4850,7 @@ bool ChatHandler::HandleResetHonorCommand (const char * args)
 
 /* [TZERO]
     player->SetUInt32Value(PLAYER_FIELD_KILLS, 0);
-    player->SetUInt32Value(PLAYER_FIELD_LIFETIME_HONORBALE_KILLS, 0);
+    player->SetUInt32Value(PLAYER_FIELD_LIFETIME_HONORABLE_KILLS, 0);
     player->SetUInt32Value(PLAYER_FIELD_HONOR_CURRENCY, 0);
     player->SetUInt32Value(PLAYER_FIELD_TODAY_CONTRIBUTION, 0);
     player->SetUInt32Value(PLAYER_FIELD_YESTERDAY_CONTRIBUTION, 0); */
