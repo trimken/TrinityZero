0fb2cd071324e1142dcf1a59f88e851e50e8a223
Maddogz <Maddogz@localhost>
2010-09-01 15:37:26 -0400

    Update copyright notice

--- src/game/OutdoorPvPMgr.cpp
+++ src/game/OutdoorPvPMgr.cpp
@@ -1,6 +1,8 @@
 /*
  * Copyright (C) 2008 Trinity <http://www.trinitycore.org/>
  *
+ * Copyright (C) 2009-2010 TrinityZero <http://www.trinityzero.org/>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
